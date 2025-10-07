f1f7c62b257303db53947efe65133a5a2c13264f
novaka-dev <vaka.coding@gmail.com>
2025-10-07 15:04:28 +0700

    feat(seachbar): create search bar in navbar dekstop, not responsife

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -10,7 +10,7 @@ const SearchBar = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md bg-white px-3 py-1 text-sm text-black shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-9 w-full rounded-md bg-white px-3 py-1 text-sm text-black shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
